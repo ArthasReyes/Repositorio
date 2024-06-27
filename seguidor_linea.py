17c9c497221683d2f0a35bed40e4d0d6654b016d
ArthasReyes <fer.reyesl@gmail.com>
2024-06-27 11:00:35 -0400

    Update seguidor_linea.py

--- seguidor_linea.py
+++ seguidor_linea.py
@@ -2,7 +2,7 @@ from machine import Pin, ADC
 from movil import Movil_2_ruedas
 
 #Motores
-autito = Autito(motorA1_pin=1, motorA2_pin=2, motorB1_pin=4, motorB2_pin=5)
+autito = Movil_2_ruedas(motorA1_pin=1, motorA2_pin=2, motorB1_pin=4, motorB2_pin=5)
 
 
 #sensores QTI
@@ -12,7 +12,7 @@ sensores = [ADC(Pin(pin)) for pin in sensores_pines]
 
 # Casos posibles: 0 blanco, 1 negro
 acciones = {
-    (0, 0, 0, 0): autito.adelante,
+    (0, 0, 0, 0): autito.avanzar,
     (1, 0, 0, 0): autito.izquierda,
     (1, 1, 0, 0): autito.izquierda,
     (0, 0, 1, 0): autito.derecha,
